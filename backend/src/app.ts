0f1d7ac1d22d6ea3e5e9860d9ca9f84dd362f932
init1-dev <init1.dev@gmail.com>
2024-07-24 19:36:12 +0200

    FIX: backend app.ts error handler in google-auth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -35,6 +35,7 @@ app.post("/google-auth", async (req, res) => {
         }
         res.status(200).json(user);
     } catch (error) {
-        
+        console.error('Error verifying token:', error);
+        res.status(400).json({ error: 'Invalid token' });
     }
 })
