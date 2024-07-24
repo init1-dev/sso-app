21c979596afd21e0964e29de949d714093904f5b
init1-dev <init1.dev@gmail.com>
2024-07-24 19:28:04 +0200

    FIX: front App component, REMOVE: passport and passport-google-oauth20 packages, ADD: google-auth-library, FIX: backend app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,63 +1,40 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { OAuth2Client } from 'google-auth-library';
 import session from 'express-session';
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import cors from 'cors';
 import config from './utils/getEnv';
 import corsOptions from './utils/corsOptions';
 
+
 export const app = express();
 
 app.use(cors(corsOptions));
+app.use(express.json());
 app.use(session({
     secret: config.SESSION_SECRET,
     resave: false,
     saveUninitialized: true
 }));
-app.use(passport.initialize());
-app.use(passport.session());
 
-passport.use(new GoogleStrategy({
-    clientID: config.GOOGLE_CLIENT_ID,
-    clientSecret: config.GOOGLE_CLIENT_SECRET,
-    callbackURL: config.CALLBACK_URL
-},
-    function (_accessToken, _refreshToken, profile, done) {
-        return done(null, profile);
-    }));
+const client = new OAuth2Client();
 
-passport.serializeUser((user: any, done) => {
-    done(null, user);
-});
-
-passport.deserializeUser((obj: any, done) => {
-    done(null, obj);
-});
-
-app.get('/auth/google',
-    passport.authenticate('google', { scope: ['profile', 'email'] })
-);
-
-app.get('/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/' }),
-    (_req: Request, res: Response) => {
-        res.redirect('/profile');
-    }
-);
-
-app.get('/profile', (req: Request, res: Response) => {
-    if (req.isAuthenticated()) {
-        res.json(req.user);
-    } else {
-        res.status(401).send('You are not authenticated');
-    }
-});
-
-app.get('/logout', (req: Request, res: Response) => {
-    req.logout((err: any) => {
-        if (err) {
-            return res.status(500).send('Logout failed');
+app.post("/google-auth", async (req, res) => {
+    const { credential, client_id } = req.body;
+    
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: credential,
+            audience: client_id
+        });
+        const payload = ticket.getPayload();
+        const user = {
+            id: payload?.sub,
+            name: payload?.name,
+            email: payload?.email,
+            picture: payload?.picture
         }
-    });
-    res.redirect('/');
-});
+        res.status(200).json(user);
+    } catch (error) {
+        
+    }
+})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,14 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
-import styled from 'styled-components'
+import router from './routes/routes'
 
 function App() {
-    const loginWithGoogle = () => {
-        window.location.href = 'http://localhost:5000/auth/google';
-    };
 
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>React & Node.js SSO Auth with Google</h1>
-
-            <GoogleButton onClick={loginWithGoogle}>Login with Google</GoogleButton>
-
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <RouterProvider router={router} />
     )
 }
 
-const GoogleButton = styled.button`
 
-`;
 
 export default App
