ca2f84f3f8b88f88c1110bfa3e3f43e2f9e5b1d1
init1-dev <init1.dev@gmail.com>
2024-07-28 23:30:19 +0200

    ADD: nginx and certbot wip

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,6 +1,9 @@
 import { Navigate } from "react-router-dom";
 import styled from "styled-components";
 import reactLogo from '../assets/react.svg'
+import googleLogo from '../assets/google.svg'
+import dockerLogo from '../assets/docker.svg'
+import nginxLogo from '../assets/nginx.svg'
 import viteLogo from '/vite.svg'
 import GoogleAuth from "../components/GoogleAuth/GoogleAuth";
 import { useState } from "react";
@@ -20,10 +23,22 @@ const MainPage = () => {
                 <a href="https://react.dev" target="_blank">
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
+                <a href="https://developers.google.com/identity/authentication" target="_blank">
+                    <img src={googleLogo} className="logo react" alt="React logo" />
+                </a>
+                <a href="https://docs.docker.com/compose/" target="_blank">
+                    <img src={dockerLogo} className="logo react" alt="React logo" />
+                </a>
+                <a href="https://hub.docker.com/_/nginx" target="_blank">
+                    <img src={nginxLogo} className="logo react" alt="React logo" />
+                </a>
             </div>
 
             <div>
-                <h1>SSO Auth with Google (React + Node.js)</h1>
+                <TitleContainer>
+                    <h1>SSO Auth with Google</h1>
+                    <h3>Main technologies: React, Node.js, Docker and Nginx</h3>
+                </TitleContainer>
 
                 {   !user
                         ? <GoogleAuth user={user} setUser={setUser}/>
@@ -42,6 +57,10 @@ const AppContainer = styled.div`
     display: flex;
     flex-direction: column;
     gap: 2rem;
+`;
+
+const TitleContainer = styled.div`
+    margin-bottom: 3rem;
 `;
 
 export default MainPage;
