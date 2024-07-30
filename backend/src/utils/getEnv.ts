8761c9dc797316375186e65917de4f0eb4001641
init1-dev <init1.dev@gmail.com>
2024-07-30 12:41:57 +0200

    FIX: remove unused env

--- backend/src/utils/getEnv.ts
+++ backend/src/utils/getEnv.ts
@@ -4,11 +4,9 @@ import { Secret } from "jsonwebtoken";
 configDotenv();
 
 const SESSION_SECRET: Secret = process.env.SESSION_SECRET || "default_secret";
-const GOOGLE_CLIENT_SECRET: Secret = process.env.GOOGLE_CLIENT_SECRET || "";
 
 const config = {
-    SESSION_SECRET: String(SESSION_SECRET),
-    GOOGLE_CLIENT_SECRET: String(GOOGLE_CLIENT_SECRET),
+    SESSION_SECRET: String(SESSION_SECRET)
 }
 
 export default config;
